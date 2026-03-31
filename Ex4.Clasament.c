82673ed1f74abde99b68232baac4241e33b54320
mariaaaa07 <gherasim.maria30@gmail.com>
2026-03-31 08:38:19 +0300

    Update Ex4.Clasament.c

--- Ex4.Clasament.c
+++ Ex4.Clasament.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-// Definim structura pentru un alergator
 typedef struct {
     char nume[50];
     float timp;
@@ -15,11 +14,9 @@ int main() {
         {"Radu", 10.01}, {"Andreea", 8.82}, {"Victor", 12.30}, {"Oana", 9.95}
     };
 
-    // Algoritm de sortare (Bubble Sort) pentru a ordona dupa timp
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (lista[j].timp > lista[j + 1].timp) {
-                // Interschimbam obiectele de tip Alergator
                 Alergator temp = lista[j];
                 lista[j] = lista[j + 1];
                 lista[j + 1] = temp;
@@ -27,11 +24,9 @@ int main() {
         }
     }
 
-    // Afisarea rezultatelor sub forma de clasament
     printf("Clasament Final:\n");
     printf("---------------------------\n");
     for (int i = 0; i < n; i++) {
-        // i + 1 reprezinta locul (indexul incepe de la 0)
         printf("Locul %d: %s\n", i + 1, lista[i].nume);
     }
 
