1735d504fa123ec6ce31515a65c1787a2fc42f5c
mariaaaa07 <gherasim.maria30@gmail.com>
2026-03-31 08:37:20 +0300

    Update Ex3.Adunarea.c

--- Ex3.Adunarea.c
+++ Ex3.Adunarea.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Functia de sortare QuickSort - Eficienta medie O(n log n)
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int pivot = arr[high];
@@ -29,20 +28,18 @@ int main() {
         return 1;
     }
 
-    // Generam numere aleatorii
     srand(time(NULL));
     for (int i = 0; i < n; i++) {
-        v[i] = rand() % 20001 - 10000; // Valori intre -10000 si 10000
+        v[i] = rand() % 20001 - 10000;
     }
 
-    // Pasul 1: Sortarea (esențiala pentru metoda Two Pointers)
+ 
     quickSort(v, 0, n - 1);
     printf("Vectorul a fost generat si sortat cu succes.\n");
 
     int sumaCautata;
     char optiune;
 
-    // Pasul 2: Bucla pentru cautari multiple
     do {
         printf("\nIntroduceti suma pe care o cautati: ");
         if (scanf("%d", &sumaCautata) != 1) break;
@@ -51,20 +48,20 @@ int main() {
         int dreapta = n - 1;
         int gasit = 0;
 
-        // Algoritmul Two Pointers - Eficienta O(n)
+
         while (stanga < dreapta) {
             int sumaCurenta = v[stanga] + v[dreapta];
 
             if (sumaCurenta == sumaCautata) {
                 printf("Pereche gasita: %d + %d = %d\n", v[stanga], v[dreapta], sumaCautata);
                 gasit = 1;
-                break; // Ne oprim la prima pereche gasita
+                break;
             } 
             else if (sumaCurenta < sumaCautata) {
-                stanga++; // Suma e prea mica, mergem spre numere mai mari
+                stanga++; 
             } 
             else {
-                dreapta--; // Suma e prea mare, mergem spre numere mai mici
+                dreapta--; 
             }
         }
 
@@ -73,11 +70,11 @@ int main() {
         }
 
         printf("Mai cautati o alta suma? (d/n): ");
-        scanf(" %c", &optiune); // Spatiul inainte de %c curata buffer-ul (tasta Enter)
+        scanf(" %c", &optiune); 
 
     } while (optiune == 'd' || optiune == 'D');
 
-    // Pasul 3: Eliberarea memoriei
+
     free(v);
     printf("Memorie eliberata. Programul s-a inchis.\n");
 
